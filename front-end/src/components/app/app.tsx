09e3e441e870cde6ddd108688f40c41ca38601b1
trungnh-aiot <trungnh@aiot-global.com>
2025-06-25 11:43:46 +0700

    test: test app file

---
+++ front-end/src/components/app/app.tsx
@@ -0,0 +1,13 @@
+export function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to the Full Stack Docker App</h1>
+        <p>
+          This is a simple application demonstrating a full stack setup with
+          Docker.
+        </p>
+      </header>
+    </div>
+  );
+}
