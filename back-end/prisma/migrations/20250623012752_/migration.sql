4f587e156394ab2dd77b407a2bb05b9bf99f8324
trungnh-aiot <trungnh@aiot-global.com>
2025-06-25 07:52:48 +0700

    fix: add prisma to repo

---
+++ back-end/prisma/migrations/20250623012752_/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE `Counter` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `count` INTEGER NOT NULL DEFAULT 0,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Note` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `content` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
